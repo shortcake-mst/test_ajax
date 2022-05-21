f5aa9179d7bfeb7e95939ae1813a387b9e6ee19d
shortcake-mst <103608858+shortcake-mst@users.noreply.github.com>
2022-05-21 21:02:37 +0900

    完成

--- ajax-day/app.js
+++ ajax-day/app.js
@@ -1,7 +1,7 @@
 $(function () {
     $("#search_btn").click(function () {
         // 入力された値を取得
-        var zipcode = $('zipcode').val();
+        var zipcode = $('#zipcode').val();
         // urlを設定
         var url = "https://zipcloud.ibsnet.co.jp/api/search";
         // 送るデータを成形する
@@ -22,7 +22,19 @@ $(function () {
                 $('#zip_result').html(res.message);
             } else {
                 //住所を表示
-                console.log(data);
+                var html = '';
+                    for (var i = 0; i < res.results.length; i++) {
+                        var result = res.results[i];
+                        // console.log(res.results);
+                        html += '<div>都道府県コード：' + result.prefcode + '</div>';
+                        html += '<div>都道府県：' + result.address1 + '</div>';
+                        html += '<div>市区町村：' + result.address2 + '</div>';
+                        html += '<div>町域：' + result.address3 + '</div>';
+                        html += '<div>都道府県(カナ)：' + result.kana1 + '</div>';
+                        html += '<div>市区町村(カナ)：' + result.kana2 + '</div>';
+                        html += '<div>町域(カナ)：' + result.kana3 + '</div>';
+                    }
+                    $('#zip_result').html(html);
             }
         })
         .fail(function (error) {
