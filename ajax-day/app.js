c96704392d6d0357d01407a5be9bd5e61d19c6ed
shortcake-mst <103608858+shortcake-mst@users.noreply.github.com>
2022-05-21 18:08:06 +0900

    Initial commit

---
+++ ajax-day/app.js
@@ -0,0 +1,33 @@
+$(function () {
+    $("#search_btn").click(function () {
+        // 入力された値を取得
+        var zipcode = $('zipcode').val();
+        // urlを設定
+        var url = "https://zipcloud.ibsnet.co.jp/api/search";
+        // 送るデータを成形する
+        var param = { zipcode: zipcode };
+        // サーバーと通信(Ajax)
+        
+        $.ajax({
+            type: "GET", 
+            cache: false,
+            data: param,
+            url: url,
+            dataType: "jsonp"
+        })
+        .done(function (res) {
+            if (res.status != 200) {
+                // 通信には成功。APIの結果がエラー
+                // エラー内容を表示
+                $('#zip_result').html(res.message);
+            } else {
+                //住所を表示
+                console.log(data);
+            }
+        })
+        .fail(function (error) {
+            console.log(error);
+            $('#zip_result').html("<p>通信エラーです。時間をおいてお試しください</p>");
+        });
+    });
+});
